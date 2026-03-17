638ad288e04e4b72f1d7a15715920f593aaf187c
Fróes <pedropfpd@gmail.com>
2026-03-17 19:21:36 -0300

    Add files via upload

---
+++ Exercícios/Silde_05/Personagem.java
@@ -0,0 +1,10 @@
+package Silde_05;
+
+public class Personagem {
+    
+    // Coordenadas do personagem e variável booleana pra morte do personagem
+    int posx = 4;
+    int posy = 4;
+    boolean morto = false;
+
+}
